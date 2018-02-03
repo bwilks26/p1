6f90f6daf21a48e5b8f497b8ba697a3305b3022f
bwilks26 <bryan@freeformagency.com>
2018-02-03 16:47:04 -0600

    Adding in index and subdirectories

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# p1
+# Project 1
++ By: *Bryan Wilks*
++ Production URL: <http://p1.bryanwilksharvard.club>
+
+## Outside resources
+*your list of outside resources go here*
+
---
+++ index.php
@@ -0,0 +1,74 @@
+<html>
+	<head>
+		<title>Bryan Wilks</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" type="text/css">
+		<link rel="stylesheet" href="css/style.css">
+	<head>
+	<body>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<h1 class="text-center name">Bryan Wilks</h1>
+			</div>
+			<div class="row-fluid">
+				<img class="profile-picture" src="images/bryan-wilks.png" alt="Bryan Wilks">
+			</div>
+			<div class="row-fluid introduction">
+				<p>
+					Bryan Wilks BA, MBA is an entrepreneur and strategy consultant. His fields of 
+					experience span from hard core technology disciplines like assisting client sites 
+					across North America, Europe and Asia in optimizing their IT strategies to sophisticated consumer marketing industries ranging from packaging to entertainment.
+				</p>
+				<p>
+					As an entrepreneur CEO, Bryan founded four companies and has equity positions in 
+					many others: Clear View Group a global packaging company, FreeForm Agency a marketing 
+					and design company working with notable entertainment brands, professional athletes, 
+					industrial manufacturing companies, and large non profit organizations, Sager Creek 
+					Packaging a procurement consulting company with four decades of collective experience, 
+					and SP Marketing a sales organization representing some of the largest manufactures in 
+					the world.
+				</p>
+				<p>	
+					As a strategy and marketing consultant, Bryan consulted with many fortune 500 companies. 
+					His work also includes numerous startups and VCs, and he played interim VP Marketing roles 
+					for several such ventures. Bryan has a proven track record in both small private companies 
+					and divisions of larger companies.
+				</p>
+				<p>
+					As the Vice President of Business Development and Director of Marketing for two of the 
+					largest privately held companies in North America he created major growth strategies 
+					through new market penetration, M&A, and industry roll-ups.
+				</p>
+				<p>
+					As a inventor he marketed and developed many well-regarded methodologies for consumer 
+					goods which he has used repeatedly in different situations and across a variety of market 
+					segments.
+				</p>
+				<p>
+					Bryan began his professional career in politics. He managed a high profile U.S. Congressional 
+					campaign in Texas, assisted U.S. House Majority Leader Dick Armey during the Contract With 
+					America , and served as the Executive Director of the Conservative Leadership Political Action 
+					Committee in Washington, D.C. where he was responsible for millions in annual contributions to 
+					US Senate, Congressional, and Presidential candidates. He graduated from the prestigious 
+					Leadership Institute (LI) where Morton Blackwell, LI’s founder, personally mentored Bryan to 
+					teach workshops on marketing, campaigns, fundraising, grassroots organizing, youth politics, 
+					and communications around the world.
+				</p>
+				<p>
+					Bryan is a college All-American, Coach C.L. Brigham Award recipient for outstanding athletic & 
+					academic achievement, and was recently selected as one of the Top 20 Leaders for being among the 
+					most prominent and successful leaders in business.
+				</p>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<h2 class="text-center">Random Quote</h2>
+			</div>
+			<div class="row-fluid">
+				<blockquote>...</blockquote>
+			</div>
+		</div>
+	</body>
+</html>
